ea0259f9b201fcdbdf474e34888205b0a561d102
RomanKolegov <kolegov_rv@mail.ru>
2022-11-29 13:37:11 +0500

    add task34

---
+++ task34/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+int[] getArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(start, end);
+    }
+    return array;
+}
+int sumOfEvenNumberDigit(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+int[] array = getArray(5,100,1000);
+printArray(array);
+int sumOfDigits = sumOfEvenNumberDigit(array);
+Console.WriteLine($"Сумма четных чисел массива равна {sumOfDigits}");
